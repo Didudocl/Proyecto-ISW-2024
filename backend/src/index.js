47b25523ba5572cb9be6edcd4faff3e1d3c2e90a
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-10-16 20:42:03 -0300

    ADD comment

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ import { connectDB } from "./config/configDb.js";
 import { createUsers } from "./config/initialSetup.js";
 import { passportJwtSetup } from "./auth/passport.auth.js";
 
+// Función asincrona para configurar nuestro servidor de express
 async function setupServer() {
   try {
     const app = express();
